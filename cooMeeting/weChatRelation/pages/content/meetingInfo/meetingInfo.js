a39f6bcca370cccc833911d84cdea2d0873258ab
ZsummerH <38202589+ZsummerH@users.noreply.github.com>
2020-03-20 18:46:11 +0800

    Add files via upload

--- cooMeeting/weChatRelation/pages/content/meetingInfo/meetingInfo.js
+++ cooMeeting/weChatRelation/pages/content/meetingInfo/meetingInfo.js
@@ -1,21 +1,61 @@
+import {
+  BOARDROOMS
+} from '../../../utils/data.js';
+
 Page({
-    data: {
-        inputShowed: false,
-        inputVal: ""
-    },
-    onLoad() {
+  data: {
+    inputShowed: false,
+    inputVal: "",
+    meetingRooms: BOARDROOMS
+  },
+  onLoad() {
+    this.setData({
+      search: this.search.bind(this)
+    })
+  },
+  search: function(value) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        resolve([])
+      }, 200)
+    })
+  },
+  selectResult: function(e) {
+    console.log('select result', e.detail)
+  },
+  viewDetail: (a) => {
+    let chooseData = a.target && a.target.dataset
+    let nowDay = new Date
+    wx.request({
+      url: 'http://127.0.0.1:8888',
+      method: 'POST',
+      dataType: 'json',
+      data: {
+        roomId: chooseData.id,
+        date: nowDay.toLocaleDateString()
+      },
+      success: rsp => {
         this.setData({
-            search: this.search.bind(this)
+          showLoading: false
         })
-    },
-    search: function (value) {
-        return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                resolve([{text: '搜索结果', value: 1}, {text: '搜索结果2', value: 2}])
-            }, 200)
-        })
-    },
-    selectResult: function (e) {
-        console.log('select result', e.detail)
-    },
+        console.log(res.data)
+      }
+    })
+    console.log(chooseData)
+  },
+  goReserve: (a) => {
+    let chooseData = a.target && a.target.dataset
+    wx.navigateTo({
+      url: '../meetingDetails/details',
+      success: function (res) {
+        // 通过eventChannel向被打开页面传送数据
+        res.eventChannel.emit('acceptDataFromOpenerPage', 
+        { 
+          roomId : chooseData.id,
+          roomName: chooseData.name
+        }
+        )
+      }
+    })
+  }
 });
